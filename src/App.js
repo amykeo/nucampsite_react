a904d91a5076be82b73273c6a477d7a3152648ce
amy <amykeophilavong@outlook.com>
2020-10-05 09:21:40 -0700

    gh pages

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <BrowserRouter>
+        <BrowserRouter basename="/nucampsite_react">
           <div className="App">
             <Main />
           </div>
